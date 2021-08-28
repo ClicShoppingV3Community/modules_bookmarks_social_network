d8b7462733453115b424f7afe10a0832eb561678
ClicShopping <admin@clicshopping.org>
2021-08-28 14:43:43 +0200

    ADD TYPAGE

--- includes/modules/social_bookmarks/sb_twitter_button.php
+++ includes/modules/social_bookmarks/sb_twitter_button.php
@@ -17,8 +17,8 @@
   class sb_twitter_button
   {
     public string $code;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
     public $icon;
